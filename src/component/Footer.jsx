39a521047f1af141bc0af5c1e5d5ba60d67803dd
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2025-08-09 18:27:15 +0500

    update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='bg-gray-800 text-white md:p-14'>
+        <div className='container mx-auto md:px-32 p-20 w-full'>
+
+          {/* Grid layout for responsiveness */}
+          <div className='grid grid-cols-1 md:grid-cols-4 gap-24'>
+
+            {/* Column 1 */}
+            <div className='md:space-y-4 space-y-2'>
+              <h2 className='text-md tracking-wide font-bold'>Hammad Raza.</h2>
+              <p className='text-sm tracking-wide'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error accusamus!
+              </p>
+            </div>
+
+            {/* Column 2 */}
+            <div className='md:space-y-4 space-y-2'>
+              <p className='text-sm tracking-wide'>Subscribe to get Important Updates</p>
+              <div className='flex flex-col space-y-2'>
+                <input type="text" className='bg-white px-1 py-2 w-full text-black pl-2' placeholder='Your Opinion' />
+                <button className='bg-blue-600 p-2 py-3 rounded-lg uppercase hover:bg-blue-800'>
+                  Subscribe
+                </button>
+              </div>
+            </div>
+
+            {/* Column 3 */}
+            <div className='md:space-y-4 space-y-2'>
+              <p className='text-md tracking-wide'>Follow Us</p>
+              <div className='flex space-x-2'>
+                <a href="#" className='border border-black rounded-full p-2 hover:rotate-y-180'><FaFacebookF /></a>
+                <a href="#" className='border border-black rounded-full p-2 hover:rotate-y-180'><FaGithub /></a>
+                <a href="#" className='border border-black rounded-full p-2 hover:rotate-y-180'><FaLinkedin /></a>
+              </div>
+            </div>
+
+            {/* Column 4 */}
+            <div className='md:space-y-4 space-y-2'>
+              <p className='text-md tracking-wide'>Call Us</p>
+              <p className='text-sm tracking-wide'>+92 316 3093120</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,16 @@ import { ImTruck } from 'react-icons/im'
 import { MdOutlineSecurity } from 'react-icons/md'
 import { FaRegMoneyBillAlt } from 'react-icons/fa'
 import { RiSecurePaymentFill } from 'react-icons/ri'
+import { FaMobileAlt } from 'react-icons/fa'
+import { MdOutlineLaptopMac, MdOutlineWatch } from 'react-icons/md'
+import { TbAirConditioning } from 'react-icons/tb'
+import { RiFridgeFill } from 'react-icons/ri'
 
 function Home() {
   return (
     <>
-      <section className='container mx-auto p-32'>
+    {/* Hero Section */}
+      <section className='container mx-auto md:p-32 p-16'>
         <div className='flex flex-col md:flex-row justify-between items-center gap-10'>
 
 
@@ -20,12 +25,12 @@ function Home() {
               Molestias, adipisci magni inventore unde odio corrupti
               libero quasi repellendus? Ipsum amet eum porro recusandae
               Ratione.</p>
-            <button className='bg-blue-700 px-6 py-3 text-white hover:bg-blue-800 transition rounded-lg'>Shop Now</button>
+            <button className='bg-blue-700 px-6 py-3  text-white hover:bg-blue-900 transition rounded-lg'>Shop Now</button>
           </div>
 
 
           {/* Right Section */}
-          <div className='relative w-full md:w-2/5 flex justify-center md:justify-end'>
+          <div className='relative w-full md:w-2/5 flex justify-center md:justify-end mb-16 md:my-0'>
             <div className='md:h-52 md:w-52 h-32 w-32 bg-green-400 absolute top-24 -right-5 md:-top-12 md:-right-8 -z-10'></div>
             <img src="https://images.unsplash.com/photo-1505460932124-165b2d436f5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZWNvbWVyY2V8ZW58MHwwfDB8fHww"
               className='relative max-w-[250px] md:max-w-[400px] top-14 md:-top-2  shadow-lg' />
@@ -44,7 +49,7 @@ function Home() {
 
           {/* card */}
           <div className='grid grid-cols-1 md:grid-cols-3 gap-8 p-14 md:p-0'>
-            <div className='relative w-full md:w-64 bg-gray-300 p-2'>
+            <div className='relative w-full md:w-64 bg-gray-300 p-2 hover:transition-all hover:scale-105 md:hover:scale-110 cursor-pointer duration-300'>
               <div className='flex flex-col'>
                 <img src="https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFwdG9wfGVufDB8fDB8fHww" alt="Service Icon" className='mb-4' />
                 {/* <img src={image} alt="Service Icon" className='mb-4' /> */}
@@ -55,7 +60,7 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className='relative w-full md:w-64 bg-gray-300 p-2'>
+            <div className='relative w-full md:w-64 bg-gray-300 p-2 hover:transition-all hover:scale-105 md:hover:scale-110 cursor-pointer duration-300'>
               <div className='flex flex-col'>
                 <img src="https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFwdG9wfGVufDB8fDB8fHww" alt="Service Icon" className='mb-4' />
                 {/* <img src={image} alt="Service Icon" className='mb-4' /> */}
@@ -66,7 +71,7 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className='relative w-full md:w-64 bg-gray-300 p-2'>
+            <div className='relative w-full md:w-64 bg-gray-300 p-2 hover:transition-all hover:scale-105 md:hover:scale-110 cursor-pointer duration-300'>
               <div className='flex flex-col'>
                 <img src="https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGFwdG9wfGVufDB8fDB8fHww" alt="Service Icon" className='mb-4' />
                 {/* <img src={image} alt="Service Icon" className='mb-4' /> */}
@@ -87,20 +92,20 @@ function Home() {
           <div className='container mx-auto md:px-32'>
             <div className='flex flex-col md:flex-row justify-between items-center gap-10 p-16' >
             {/* card 1 */}
-             <div className='w-full md:w-72 md:h-64 p-12 md:p-0 bg-gray-300 rounded-2xl flex flex-col justify-center items-center space-y-4'>
+             <div className='w-full md:w-72 md:h-64 p-10 md:p-0 bg-gray-300 rounded-2xl flex flex-col justify-center items-center space-y-4 hover:transition-all hover:scale-110 cursor-pointer duration-300'>
                  <ImTruck size={28} color='green' />
                 <span className='text-md tracking-wide '>Super Fast & Free Delivery</span>
              </div>
 
-              <div className='flex flex-col justify-between items-center gap-10'>
+              <div className='flex flex-col justify-between items-center gap-10 '>
               {/* card 2 */}
-                <div className='w-full md:w-72 md:h-28 p-12 md:p-0 bg-gray-300 rounded-2xl flex  gap-6 justify-center items-center space-y-4'>
+                <div className='w-full md:w-72 md:h-28 p-6 md:p-0 bg-gray-300 rounded-2xl flex  gap-6 justify-center items-center space-y-4 hover:transition-all hover:scale-110 cursor-pointer duration-300'>
                  <MdOutlineSecurity  size={28}  color='green' />
                 <span className='text-md tracking-wide '>Non Contract Shipping</span>
                 </div>
 
               {/* card 3 */}
-                <div className='w-full md:w-72 md:h-28 p-12  md:p-0 bg-gray-300 rounded-2xl flex gap-6 justify-center items-center space-y-4'>
+                <div className='w-full md:w-72 md:h-28 p-6  md:p-0 bg-gray-300 rounded-2xl flex gap-6 justify-center items-center space-y-4 hover:transition-all hover:scale-110 cursor-pointer duration-300'>
                  <FaRegMoneyBillAlt size={28} color='green' />
                 <span className='text-md tracking-wide '>Money Back Guaranted</span>
                 </div>
@@ -108,13 +113,51 @@ function Home() {
               </div>
 
               {/* card 4 */}
-               <div className='w-full md:w-72 md:h-64 p-12 md:p-0 bg-gray-300 rounded-2xl flex flex-col justify-center items-center space-y-4'>
+               <div className='w-full md:w-72 md:h-64 p-10 md:p-0 bg-gray-300 rounded-2xl flex flex-col justify-center items-center space-y-4 hover:transition-all hover:scale-110 cursor-pointer duration-300'>
                  <RiSecurePaymentFill size={28} color='green' />
                 <span className='text-md tracking-wide '>Super Secure Payment System </span>
                </div>
                 
               </div>
           </div>
+      </section>
+
+
+       {/* section 4  */}
+       <section className='bg-gray-100'>
+        <div className='container mx-auto md:px-32 p-6' >
+             <div>
+                 <div className='md:p-16 flex flex-col justify-center items-center space-y-20 p-10'>
+                    <span className='text-xl tracking-wide text-center'>Trusted by over 1000+ customers</span>
+                  <div className='grid md:grid-cols-5 grid-cols-2 flex-col gap-14 md:gap-x-14 '>
+                    <div className='flex flex-col md:flex-row justify-center items-center gap-1 text-gray-600 hover:transition-all hover:scale-125 cursor-pointer duration-300'>
+                       <FaMobileAlt size={28} />
+                       <span className='text-xl font-bold tracking-wide text-gray-600'>Apple</span>
+                    </div>
+
+                    <div className='flex flex-col md:flex-row justify-center items-center gap-1 text-gray-600 hover:transition-all hover:scale-125 cursor-pointer duration-300'>
+                       <MdOutlineLaptopMac size={28} />
+                       <span className='text-xl font-bold tracking-wide text-gray-600'>Hp</span>
+                    </div>
+
+                    <div className='flex flex-col md:flex-row justify-center items-center gap-1 text-gray-600 hover:transition-all hover:scale-125 cursor-pointer duration-300'>
+                       <MdOutlineWatch size={28} />
+                       <span className='text-xl font-bold tracking-wide text-gray-600'>Galaxy</span>
+                    </div>
+
+                    <div className='flex flex-col md:flex-row justify-center items-center gap-1 text-gray-600 hover:transition-all hover:scale-125 cursor-pointer duration-300'>
+                       <TbAirConditioning size={28} />
+                       <span className='text-xl font-bold tracking-wide text-gray-600'>Gree</span>
+                    </div>
+
+                    <div className='flex flex-col md:flex-row justify-center items-center gap-1 text-gray-600 hover:transition-all hover:scale-125 cursor-pointer duration-300'>
+                       <RiFridgeFill size={28} />
+                       <span className='text-xl font-bold tracking-wide text-gray-600'>Dawlance</span>
+                    </div>
+                   </div>
+                 </div>
+             </div>
+        </div>
       </section>
     </>
   )
